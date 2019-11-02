f6b84d477d9ab3ea0270ff95492b55ef0b3f0a3e
Samuel Boateng <kaybeta500@gmail.com>
2019-11-02 09:24:08 +0000

    added src dir

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './public/js/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js',
